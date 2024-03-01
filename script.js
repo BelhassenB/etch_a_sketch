dd48694ca86561cd94b89356a4fe7265e0653992
Belhassen <152809495+BelhassenB@users.noreply.github.com>
2024-03-01 23:38:06 +0100

    Add hoverinf function to the user input grid

--- script.js
+++ script.js
@@ -4,24 +4,32 @@ const body = document.querySelector('body')
 body.appendChild(mainDiv)
 
 
+
 for (let i=0; i<256; i++) {
     const squares = document.createElement('div')
     squares.setAttribute('class', 'squares')
     mainDiv.appendChild(squares)
+   
 
 }
-
-
 const hoverSquares = document.querySelectorAll('.squares')
-hoverSquares.forEach(square => {
+
+
+function hovering (container){    
+    container.forEach(square => {
     square.addEventListener("mousemove", () => {
         square.style.backgroundColor = "yellow"
     })
     
-})
+})}
+hovering(hoverSquares)
+
 
 const btn = document.createElement('button')
-btn.textContent = "ALERT ME"
+btn.textContent = "Change Grid"
+btn.style.fontWeight = "bold"
+btn.style.fontSize = "18px"
+btn.style.width = "max-content"
 body.insertBefore(btn, mainDiv)
 
 
@@ -29,7 +37,7 @@ function newGrid (){
     btn.addEventListener('click', () => {
     let gridNum = prompt('Enter the number of grids (MAX 100)', "");
     const width = 700;
-    const height = 500;
+    const height = 500;    
 
     while (mainDiv.firstChild){
         mainDiv.removeChild(mainDiv.firstChild)
@@ -37,17 +45,26 @@ function newGrid (){
      
     for (let i=0; i<(gridNum*gridNum); i++) {        
         const createGrid = document.createElement('div')
-        createGrid.setAttribute("class", "squares") 
+        createGrid.setAttribute("class", "new-squares") 
         mainDiv.appendChild(createGrid)
-        createGrid.style.width = width/gridNum
-        createGrid.style.height = height/gridNum
-        
+            
+        createGrid.style.boxSizing = "border-box"
+        createGrid.style.border = "1px white solid"
+        createGrid.style.width = width/gridNum + "px"
+        createGrid.style.height = height/gridNum + "px"
+        createGrid.style.flexGrow = "1"
+        createGrid.style.borderRadius = "20px"
+        createGrid.style.border = "1px white solid"      
+            
+    }
+    
+
     while (isNaN(gridNum) || gridNum > 100) {
         gridNum = prompt('Only numbers less than 100 are accepted', "");
-    }}   
-    
+    }
+    const hoverNewDivs = document.querySelectorAll('.new-squares')  
+    hovering(hoverNewDivs)
     
 
 })}
-console.log(newGrid())
-
+newGrid()
